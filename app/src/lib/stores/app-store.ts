066efcc68007fa44cf88c9e3f0be17990fde609c
Brendan Forster <github@brendanforster.com>
2018-10-26 16:45:59 -0300

    and some more cleanup

--- app/src/lib/stores/app-store.ts
+++ app/src/lib/stores/app-store.ts
@@ -992,14 +992,10 @@ export class AppStore extends TypedBaseStore<IAppState> {
       return
     }
 
-    const performFailableOperation = createFailableOperationHandler(
-      repository,
-      this.emitError
-    )
-
-    const changedFiles = await performFailableOperation(() =>
+    const changedFiles = await this.withErrorHandling(repository, () =>
       getChangedFiles(repository, currentSHA)
     )
+
     if (!changedFiles) {
       return
     }
@@ -1756,12 +1752,7 @@ export class AppStore extends TypedBaseStore<IAppState> {
     })
 
     const result = await this.isCommitting(repository, () => {
-      const performFailableOperation = createFailableOperationHandler(
-        repository,
-        this.emitError
-      )
-
-      return performFailableOperation(async () => {
+      return this.withErrorHandling(repository, async () => {
         const message = await formatCommitMessage(repository, context)
         return createCommit(repository, message, selectedFiles)
       })
